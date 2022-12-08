a1c205384c0ffd57102c9acd38815af779b0879c
Chrissy Cooper <cooper.christine.r@gmail.com>
2022-12-08 09:09:44 -0600

    Pass first 5 dragon.js tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,12 @@
+class Dragon {
+    constructor(name, rider){
+        this.name = name
+        this.rider = rider
+    }
 
+    greet(){
+        return `Hi, ${this.rider}!`
+    }
+}
+
+module.exports = Dragon;
