f4343bac1f67f902b87dfca5ebc08bedd0b65648
Chrissy Cooper <cooper.christine.r@gmail.com>
2022-12-08 09:20:26 -0600

    Pass dragon.js tests .eat()

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,12 +1,22 @@
 class Dragon {
     constructor(name, rider){
-        this.name = name
-        this.rider = rider
+        this.name = name;
+        this.rider = rider;
+        this.hungry = true;
+        this.hungerLevel = 0;
     }
 
     greet(){
         return `Hi, ${this.rider}!`
     }
+
+    eat(){
+        this.hungerLevel += 1;
+        if(this.hungerLevel >= 3){
+            this.hungry = false;
+        }
+    }
+
 }
 
 module.exports = Dragon;
