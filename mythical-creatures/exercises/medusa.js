b392bba0ee9c43ad5e7916a07234dcb3b06feaad
Chrissy Cooper <cooper.christine.r@gmail.com>
2022-12-13 13:14:08 -0600

    Begin medusa test suite

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,19 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+    constructor(name) {
+        this.name = name;
+        this.statues = [];
+    }
+
+    gazeAtVictim(victim) {
+        //take person class and make them a statue before adding them to an array?
+        //adding that victim to the statues array
+        //get the length of medusa.statues
+        var newStatue = new Statue(victim.name);
+        this.statues.push(newStatue);
+    }
+};
+
+module.exports = Medusa;
