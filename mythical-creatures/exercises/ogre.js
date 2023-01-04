cdd55b1d6b504c6d5ad346af997b4817dbf3d719
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-01-03 21:28:33 -0600

    Pass all ogre tests

---
+++ mythical-creatures/exercises/ogre.js
@@ -0,0 +1,27 @@
+class Ogre {
+    constructor(ogreObj) {
+        this.name = ogreObj.name;
+        this.home = ogreObj.abode || 'Swamp';
+        this.swings = 0;
+    }
+
+    encounter(humanObj) {
+        humanObj.encounterCounter += 1;
+        if(humanObj.encounterCounter % 3 === 0){
+            this.swings++;
+        } 
+        if(this.swings === 2) {
+            humanObj.knockedOut = true;
+        }
+    };
+
+    swingAt() {
+        this.swings++;
+    }
+
+    apologize(humanObj) {
+        humanObj.knockedOut = false;
+    }
+}
+
+module.exports = Ogre;
