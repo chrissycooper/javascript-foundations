b2a7fc46b2bc364d7ca872e52220d0bb8729c949
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-01-08 14:37:30 -0600

    Pass all golf-course tests

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,5 +1,25 @@
 class GolfCourse {
+    constructor(name, difficulty, openings, featuresArr) {
+        this.name = name;
+        this.difficulty = difficulty;
+        this.openings = openings;
+        this.features = featuresArr; 
+        this.openings = openings;
+        this.currentlyPlaying = [];
+    }
 
+    checkInGroup(groupArr) {
+        if (groupArr.length <= this.openings) {
+            this.openings -= groupArr.length;
+            for (var i = 0; i < groupArr.length; i++) {
+                this.currentlyPlaying.unshift(groupArr[i].name);
+            } 
+            return 'You\'re checked in. Have fun!';
+        } else {
+            return 'Sorry, we are currently booked! Please come back later.'
+        }
+
+    }
 
 }
 
--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,4 +1,49 @@
 class Golfer {
+    constructor(golferObj) {
+        this.name = golferObj.name;
+        this.handicap = golferObj.handicap;
+        this.frustration = 0;
+        this.confidence = 0;
+    }
+
+    calculateAvg(par) {
+        var avg = par + this.handicap;
+        return `I usually shoot a ${avg} on average.`
+    }
+
+    playRound(golfCourseObj) {
+        if (golfCourseObj.difficulty === 'moderate') {
+            this.frustration += 100;
+        } else if (golfCourseObj.difficulty === 'hard') {
+            this.frustration += 500;
+        }
+    }
+
+    hitTheRange() {
+        this.confidence += 10;
+    }
+
+    marvel(golfCourseObj) {
+        return `I love the ${golfCourseObj.features[0]} and ${golfCourseObj.features[1]} on this course!`;
+    }
+
+    whatYaShoot(num) {
+        if(num > 0) {
+            this.frustration += 20;
+        } else if (num === 0) {
+            this.frustration -= 10;
+        } else if (num < 0) {
+            this.frustration = 0;
+        };
+
+        if(this.frustration === 0) {
+            return 'I AM THE GREATEST GOLFER ALIVE!';
+        } else if (this.frustration === 10) {
+            return 'Booyah!';
+        } else if (this.frustration >= 20) {
+            return 'Doh!';
+        };
+    }
 
 
 }
