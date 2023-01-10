a15664c672635e40c89624309f0d113f8ffd2d4f
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-01-09 21:58:57 -0600

    Pass all Snowman and Human tests

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,6 +1,41 @@
 var Snowman = require('./Snowman');
 
 class Human {
+    constructor(name) {
+        this.name = name;
+        this.wantsToBuildASnowman = true;
+        this.materials = {
+            snowballs: 0,
+            coal: 0,
+            buttons: 0,
+            carrots: 0
+        };
+    }
+
+    gatherMaterials(material, quantity) {
+        if (material === 'carrots') {
+            this.materials.carrots += quantity;
+        } else if (material === 'buttons') {
+            this.materials.buttons += quantity;
+        } else if (material === 'coal') {
+            this.materials.coal += quantity;
+        } else if (material === 'snowballs') {
+            this.materials.snowballs += quantity;
+        }
+    }
+
+    buildASnowman() {
+        var snowman = new Snowman({carrots: this.materials.carrots, coal: this.materials.coal, buttons: this.materials.buttons, snowballs: this.materials.snowballs});
+        return snowman;
+    }
+
+    placeMagicHat(snowmanObj) { 
+       if(snowmanObj.canWearMagicHat()){
+           return 'Woah, this snowman is coming to life!'
+       } else {
+        return 'I guess I didn\'t build it correctly.'
+       }
+    }
 
 }
 
--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,20 @@
 class Snowman {
+    constructor(snowmanObj) {
+        this.carrots = snowmanObj.carrots;
+        this.coal = snowmanObj.coal;
+        this.buttons = snowmanObj.buttons;
+        this.snowballs = snowmanObj.snowballs;
+        this.magicHat = false;
+    }
 
+    canWearMagicHat(){
+        if (this.coal > 1 && this.buttons > 4 && this.carrots > 0 && this.snowballs > 2) {
+            this.magicHat = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 module.exports = Snowman;
