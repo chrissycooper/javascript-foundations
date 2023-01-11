238686feb9c2e3e1cd500a90d43ff758e8ad9448
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-01-10 21:00:28 -0600

    Pass all craft and materials tests

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,19 @@
 class Craft {
+    constructor(craftObj) {
+        this.name = craftObj.type;
+        this.materials = craftObj.materials;
+        this.completed = false;
+    }
 
+    completeCraft() {
+        this.completed = true;
+        return 'All done! It looks great!';
+    }
+
+    calculateProjectTotal() {
+    
+       return this.materials[0].calculateMaterialCost() + this.materials[1].calculateMaterialCost();
+    }
 }
 
 module.exports = Craft;
