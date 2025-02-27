0730393a060c7c015364be70a16710ec60453eb9
Chrissy Cooper <cooper.christine.r@gmail.com>
2025-02-27 15:12:31 -0600

    Pass all Lunchbox tests

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,27 @@
 class LunchBox {
+  constructor(lunchBox){
+    this.owner = lunchBox.owner;
+    this.material = lunchBox.madeOf;
+    this.shape = lunchBox.shape;
+    this.color = lunchBox.color;
+    this.snacks = [];
+  }
 
+  acquireSnack(snack){
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  }
+
+  findHealthySnacks(){
+   const healthySnacks = this.snacks.reduce((acc, snack) => {
+      if (snack.checkForHealthy()) {
+        acc.push(snack.type);
+      }
+      return acc;
+    }, []);
+
+    return healthySnacks;
+  }
 }
 
 module.exports = LunchBox;
